9deeec9f36b22616f308db48d1033bc25600d524
xuxiangyang <xxy@creatingev.com>
2015-03-10 21:48:30 +0800

    baics  proxy test get

--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -1,1 +1,25 @@
 package proxy
+
+import (
+	"logx"
+	"net/http"
+	"net/url"
+)
+
+func TestGet(proxyAddress, testSite string, validFunc func(http.Response) bool) bool {
+	proxyUrl, err := url.Parse("http://" + proxyAddress)
+	if err != nil {
+		logx.Info(proxy.Address, "not valid with error")
+		logx.Error(err)
+		return false
+	}
+
+	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+	resp, err := client.Get(testSite)
+
+	if err != nil || resp.StatusCode != 200 {
+		return false
+	}
+
+	return validFunc(resp)
+}
