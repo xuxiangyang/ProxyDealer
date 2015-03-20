d53810bc25c793dd8fae9ea862502473aafa6a01
xuxiangyang <xxy@creatingev.com>
2015-03-20 20:18:52 +0800

    connect test

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"connect"
 	"fmt"
+	"net/url"
 	"store"
 )
 
@@ -11,6 +13,15 @@ func main() {
 
 	hs := &store.RedisHashArray{}
 	test2(hs)
+
+	proxy, err := url.Parse("http://101.251.211.234:80")
+	if err != nil {
+		panic(proxy)
+	}
+
+	time, ok := connect.Test(proxy)
+	fmt.Println(time)
+	fmt.Println(ok)
 }
 
 func test(s store.SetStringStorer) {
---
+++ src/connect/connect.go
@@ -0,0 +1,83 @@
+package connect
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"logx"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	TestUrl = `http://211.155.88.207:9191/`
+)
+
+type responseJson struct {
+	Success bool `json:success`
+}
+
+func Test(proxy *url.URL) (time int, ok bool) {
+	getTime, getOk := testGet(proxy)
+	postTime, postOk := testPost(proxy)
+	return max(getTime, postTime), getOk && postOk
+}
+
+func testGet(proxy *url.URL) (time int, ok bool) {
+	getFunc := func(client *http.Client) (*http.Response, error) {
+		return client.Get(TestUrl)
+	}
+	return proxyedResponseTest(proxy, getFunc, respValidFunc)
+}
+
+func testPost(proxy *url.URL) (time int, ok bool) {
+	postFunc := func(client *http.Client) (*http.Response, error) {
+		jsonStr := []byte(`{"data":"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}`)
+		return client.Post(TestUrl, "json", bytes.NewBuffer(jsonStr))
+	}
+	return proxyedResponseTest(proxy, postFunc, respValidFunc)
+}
+
+func respValidFunc(resp *http.Response) bool {
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return false
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logx.Error(err)
+		return false
+	}
+
+	var rj responseJson
+	err = json.Unmarshal(body, &rj)
+	if err != nil {
+		logx.Error(err)
+		return false
+	}
+	return rj.Success
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func proxyedResponseTest(proxy *url.URL, fetchFunc func(*http.Client) (*http.Response, error), validFunc func(*http.Response) bool) (int, bool) {
+	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxy)}}
+
+	startTime := time.Now()
+	resp, err := fetchFunc(client)
+	endTime := time.Now()
+	if err != nil || !validFunc(resp) {
+		logx.Error(err)
+		return -1, false
+	}
+	return int(endTime.Sub(startTime).Seconds()), true
+}
